a18df4b52fcd62c3bbec46c1276f434bef74d856
dev.cunningham <dev.jon.cunningham@gmail.com>
2026-07-21 18:48:15 -0500

    Install contextual map generation at application startup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { installContextualMapGeneration } from "./app/installContextualMapGeneration.js";
+import { MapEngine } from "./map/engine/MapEngine";
 import "./index.css";
+
+installContextualMapGeneration(MapEngine);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
