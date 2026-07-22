c2222b5eee49eefe0274b8b410b2fc379c97901a
dev.cunningham <dev.jon.cunningham@gmail.com>
2026-07-21 20:50:08 -0500

    Remove redundant contextual stage wrapper

--- src/components/ContextualTacticalStage.jsx
+++
@@ -1,6 +0,0 @@
-import { installContextualMapEngineGeneration } from "../app/contextualMapEngineIntegration.js";
-import { MapEngine } from "../map/engine/MapEngine";
-
-installContextualMapEngineGeneration(MapEngine);
-
-export { TacticalStage } from "./TacticalStage";
