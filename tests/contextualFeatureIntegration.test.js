f83ca3a84c5e480e51409266af26bd393ef4a732
dev.cunningham <dev.jon.cunningham@gmail.com>
2026-07-22 23:54:33 -0500

    Wrap contextual feature integration import

--- tests/contextualFeatureIntegration.test.js
+++ tests/contextualFeatureIntegration.test.js
@@ -1,5 +1,7 @@
 import { describe, expect, it, vi } from "vitest";
-import { installContextualMapEngineGeneration } from "../src/app/contextualMapEngineIntegration.js";
+import {
+  installContextualMapEngineGeneration,
+} from "../src/app/contextualMapEngineIntegration.js";
 
 const result = {
   plan: {
