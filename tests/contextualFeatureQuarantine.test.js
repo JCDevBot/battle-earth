5f0ae81a18c088324ecad5de2bab096d205c7c4a
dev.cunningham <dev.jon.cunningham@gmail.com>
2026-07-22 23:54:48 -0500

    Wrap contextual quarantine import

--- tests/contextualFeatureQuarantine.test.js
+++ tests/contextualFeatureQuarantine.test.js
@@ -1,5 +1,7 @@
 import { describe, expect, it, vi } from "vitest";
-import { quarantineSuspiciousWaterFeatures } from "../src/app/contextualFeatureQuarantine.js";
+import {
+  quarantineSuspiciousWaterFeatures,
+} from "../src/app/contextualFeatureQuarantine.js";
 
 function suspicious(index, reason = "suspicious-full-map-coverage") {
   return { collection: "water", index, reason, sourceId: `water-${index}` };
